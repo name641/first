1b237f37f20cc3516cd20fce42acb9457dcf5d58
name641 <gongbenyou89@gmail.com>
2026-05-28 09:29:39 +0900

    Fix duplicate test class

--- laravel/tests/Feature/EnvironmentTest.php
+++ laravel/tests/Feature/EnvironmentTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class TaskTest extends TestCase
+class EnvironmentTest extends TestCase
 {
     use RefreshDatabase;
 
